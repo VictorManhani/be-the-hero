d6e9366bed210936c93ce75bfdfd4819cc9119e4
Manhani <victor.manhani@capgemini.com>
2020-03-25 14:49:47 -0300

    Front-end do app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './global.css';
+import Routes from './routes';
+
+// JSX (JavasScript & XML)
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
